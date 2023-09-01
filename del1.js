618048ad9d32aa117798c46588fc0d85b8645148
ChasHusse <115458547+ChasHusse@users.noreply.github.com>
2023-09-01 13:31:19 +0200

    Update del1.js

--- del1.js
+++ del1.js
@@ -1,27 +1,42 @@
-class Color {
-    constructor(r, g, b){
-        this.r = r
-        this.g = g
-        this.b = b
-    };
+console.log("COLOR START")
 
-    rgb(){
-        console.log(`rgb(${this.r}, ${this.g}, ${this.b})`)
-    };
 
-    hex(){
-        const rgbToHex = (r, g, b) => '#' + [r, g, b]
-        .map(x => x.toString(16).padStart(2, '0')).join('')
-        
-        console.log("hex: " + rgbToHex(this.r, this.g, this.b));
-    };
 
-    rgba(alpha){
-        console.log(`rgba(${this.r}, ${this.g}, ${this.b}, ${alpha})`)
-    };
-};
+let bodyBackgroundColor = document.querySelector("body")
 
-let colorOrange = new Color(255, 165, 0);
-colorOrange.rgb();
-colorOrange.hex();
-colorOrange.rgba(50);
+function Colors(r, g, b){
+    this.r = r
+    this.g = g
+    this.b = b
+}
+
+Colors.prototype.rgb = function (){
+    return `rgb(${this.r}, ${this.g}, ${this.b})`
+}
+
+Colors.prototype.hex = function (){
+    let r = this.r.toString(16)
+    let g = this.g.toString(16)
+    let b = this.b.toString(16)
+
+    if (r.length === 1) r = '0' + r
+    if (g.length === 1) g = '0' + g
+    if (b.length === 1) b = '0' + b
+
+    return `#${r}${g}${b}`
+}
+
+Colors.prototype.rgba = function(a) {
+    return `rgba(${this.r}, ${this.g}, ${this.b}, ${a})`;
+}
+
+
+
+let colorOrange = new Colors(255, 165, 0);
+
+console.log(`rgb: ${colorOrange.rgb()}. hex: ${colorOrange.hex()}. rgba: ${colorOrange.rgba(1)}.`)
+
+bodyBackgroundColor.style.backgroundColor = colorOrange.rgb();
+
+console.log("COLOR END")
+
